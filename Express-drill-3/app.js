2655c0c317c53c28c17d6e992fadeb19b376b47a
kkuberra <34526935+kkuberra@users.noreply.github.com>
2018-06-20 11:37:17 -0600

    corrected

---
+++ Express-drill-3/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const data = require("./studentData.json");
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+
+function findById(id) {
+  return data.find(item => item.id === +id);
+}
+
+app.get("/", (req, res, next) => {
+  res.json({ data });
+});
+
+app.get("/:id", (req, res, next) => {
+  const item = findById(req.params.id);
+  if (!item) {
+    res.status(404).json({
+      error: {
+        message: "Item not found"
+      }
+    });
+    return;
+  }
+
+  res.json({
+    data: item
+  });
+});
+
+app.listen(port);
